3df42e8474e46b0d7c83b442454c209044e66f9c
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-09-05 18:08:30 +0700

    dsadasd

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,49 @@
 <template>
   <v-app dark>
+          <v-card>
+    <v-tabs 
+      v-model="tab"
+      background-color="black"
+      icons-and-text
+      fixed-tabs
+    >
+      <v-tabs-slider
+      color="red darken-4"
+      ></v-tabs-slider>
+      <v-tab to="/" nuxt class=" red--text text--darken-4" href="#tab-1" >
+          Logo
+      </v-tab>
+      <v-tab  to="/" nuxt class=" red--text text--darken-4"  href="#tab-2">
+        MARKET
+      </v-tab>
 
-        <Nuxt />
+      <v-tab to="/auth/inventory" nuxt class=" red--text text--darken-4"  href="#tab-3">
+        INVENTORY
+      </v-tab>
 
+      <v-tab to="/auth/inventory" nuxt class=" red--text text--darken-4"  href="#tab-4">
+        SELL
+      </v-tab>
+            <v-tab to="/auth/login" nuxt class=" red--text text--darken-4"  href="#tab-5">
+          Login
+      </v-tab>
+            <v-tab to="/auth/register" nuxt class=" red--text text--darken-4"  href="#tab-6">
+          Register
+      </v-tab>
+
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="i in 6"
+        :key="i"
+        :value="'tab-' + i"
+      >
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+
+    <nuxt/>
   </v-app>
 </template>
 
@@ -10,6 +51,9 @@
 export default {
   data () {
     return {
+       tab: null,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      
       clipped: false,
       drawer: false,
       fixed: false,
---
+++ pages/auth/register.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <h1 class="text-md-center">Register</h1>
+    <v-row dense class="mb-6" justify="center" no-gutters>
+      <v-col lg="4"></v-col>
+      <v-col lg="4" class="text-md-center"
+        ><v-text-field label="Email" outlined></v-text-field>
+      </v-col>
+      <v-col lg="4"></v-col>
+    </v-row>
+    <v-row dense no-gutters justify="center">
+      <v-col lg="4" class="text-md-center"
+        ><v-text-field 
+            v-model="password"
+            :append-icon="password1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="password1 ? 'text' : 'password'"
+            name="input-10-1"
+            hint="At least 8 characters"
+            counter
+            label="Password" outlined
+            @click:append="password1 = !password1"
+            >
+            
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row dense no-gutters justify="center">
+      <v-col lg="4" class="text-md-center"
+        ><v-text-field
+         v-model="ConfirmPassword"
+            :append-icon="password2 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.Match]"
+            :type="password2 ? 'text' : 'password'"
+            name="input-10-1"
+            hint="Password not Match"
+            counter
+            label="Confirm Password" outlined
+            @click:append="password2 = !password2"
+           ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row dense no-gutters justify="center">
+      <v-col lg="4" class="text-md-center"
+        ><v-text-field label="Name" outlined></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row dense no-gutters justify="center">
+      <v-col lg="4" class="text-md-center"
+        ><v-text-field label="Tel" outlined></v-text-field>
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        password1: false,
+        password2: false,
+        password: '',
+        ConfirmPassword:'',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          Match: v => v === this.password || 'Password not Match',
+        },
+      }
+    },
+  }
+</script>
