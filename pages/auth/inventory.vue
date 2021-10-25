d751cbf4432253cd5af56986a181c3a5b53f4931
Bruh <56395170+bankhubjar@users.noreply.github.com>
2021-10-26 01:46:22 +0700

    test

--- pages/auth/inventory.vue
+++ pages/auth/inventory.vue
@@ -73,7 +73,7 @@ export default {
   data() {
     return {
       itemInventory: '',
-      dataimage: '../../assets/weapons/Hello.png',
+      dataimage: '/usr/src/app/assets/weapons/Hello.png',
       data: {
         WeaponSkin: {
           Weapon: { WeaponName: '' },
